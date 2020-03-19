be47c439d5633b911fc21bae5f091ccf35f4bca1
ryuuki <58416474+olqlinzky@users.noreply.github.com>
2020-03-19 21:52:29 +0800

    Update index.js

--- index.js
+++ index.js
@@ -92,7 +92,7 @@ client.on("message", async (message) => {
 });
 
 // function which log in the bot
-client.login("NjkwMDE4OTIzNzQzMjgxMzUz.XnN2JQ.HB-H0FDkhx8K9qxhcJesepPl13Q");
+client.login("NjkwMDE4OTIzNzQzMjgxMzUz.XnN46A.QyxuunxfQ2x7NfmY5ZJRMfw1XPw");
 
 
 
