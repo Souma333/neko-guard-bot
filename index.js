5f82321abc59258acdca97ae9c2974c21f4dcf45
ryuuki <58416474+olqlinzky0623@users.noreply.github.com>
2020-06-15 10:09:18 +0800

    Update index.js

--- index.js
+++ index.js
@@ -45,8 +45,8 @@ bot.on("message", async (msg) => {
          }
          if(command === "help"){
              let embed = new Discord.MessageEmbed() //buat set embed discord card message
-                 .addField(":hello", "=> Biar Nggk Merasa Bosen :kissing_heart: ")
-                 .addField(":help", "=> Biar Nggk Kesasar :kissing_heart:")
+                 .addField(":beb", "Biar Nggk Merasa Bosen :kissing_heart: ")
+                 .addField(":help", "Biar Nggk Kesasar :kissing_heart:")
                  .setTitle("Commands:")
                  .setFooter("uWu")
                  .setColor("ORANGE");
